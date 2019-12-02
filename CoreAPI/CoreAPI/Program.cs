b5b64f05643c457c14868814098fb23817ac7ccc
姚远 <yaoyuan@sto.cn>
2019-12-02 15:33:28 +0800

    添加Apollo配置

---
+++ CoreAPI/CoreAPI/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Com.Ctrip.Framework.Apollo;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CoreAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                    // ¼ÓÔØApolloÅäÖÃ
+                    .ConfigureAppConfiguration((builder)=> {
+                        var configuration = builder.Build();
+
+                        var isUseApollo = configuration.GetValue("Apollo:UseApolloConfigCenter", false);
+
+                        if (isUseApollo)
+                        {
+                            builder.AddApollo(builder.Build().GetSection("Apollo"))
+                            .AddDefault()
+                            .AddNamespace("TEST1.Consul");
+                        }
+                    })
+                    ;
+                });
+    }
+}
